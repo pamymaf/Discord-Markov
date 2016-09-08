f5b501a6fc22c3fa3e010d42773a5affd1e4d042
Andrea Garcia <andrea@anodium.net>
2016-09-08 09:37:36 -0400

    Change os._exit to sys.exit

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 import configparser
-import os
+import sys
 
 class Config:
     def __init__(self):
         config = configparser.ConfigParser()
         if not config.read('config/config.ini', encoding='utf-8'):
             print("Config file not found")
-            os._exit(1)  # Exit the script
+            sys.exit(1)  # Exit the script
 
         config = configparser.ConfigParser(interpolation=None)
         config.read('config/config.ini', encoding='utf-8')
